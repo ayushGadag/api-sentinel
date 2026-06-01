49131b92ec898201cd2f32ac23e3c991656eba6b
Ayush <ayushgadag09@gmail.com>
2026-06-01 20:35:50 +0530

    Add basic API request functionality

---
+++ api-sentinel/main.py
@@ -0,0 +1,25 @@
+import requests
+from rich import print
+# API URL
+url = "https://jsonplaceholder.typicode.com/users"
+
+print("[yellow]Sending API Request...[/yellow]")
+
+try:
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print("[green]✓ API Request Successful[/green]")
+
+        data = response.json()
+
+        print("\n[cyan]First User Details:[/cyan]")
+        print(f"Name : {data[0]['name']}")
+        print(f"Email: {data[0]['email']}")
+
+    else:
+        print(f"[red]✗ API Failed[/red]")
+        print(f"Status Code: {response.status_code}")
+
+except Exception as e:
+    print(f"[bold red]Error:[/bold red] {e}")
