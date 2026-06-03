e734591c033a5ca3eb80eb5e40f657a432802fbd
Ayush <ayushgadag09@gmail.com>
2026-06-03 23:03:18 +0530

    Add response time measurement and API validation

--- api-sentinel/main.py
+++ api-sentinel/main.py
@@ -1,25 +1,51 @@
 import requests
+import time
 from rich import print
 
-url = "https://jsonplaceholder.typicode.com/users"
 
-print("[yellow]Sending API Request...[/yellow]")
+def check_api():
 
-try:
-    response = requests.get(url)
+    url = "https://jsonplaceholder.typicode.com/users"
 
-    if response.status_code == 200:
-        print("[green]✓ API Request Successful[/green]")
+    print("[yellow]Sending API Request...[/yellow]")
 
-        data = response.json()
+    try:
+        # Start Timer
+        start_time = time.time()
 
-        print("\n[cyan]First User Details:[/cyan]")
-        print(f"Name : {data[0]['name']}")
-        print(f"Email: {data[0]['email']}")
+        # Send Request
+        response = requests.get(url)
 
-    else:
-        print(f"[red]✗ API Failed[/red]")
-        print(f"Status Code: {response.status_code}")
+        # End Timer
+        end_time = time.time()
 
-except Exception as e:
-    print(f"[bold red]Error:[/bold red] {e}")
+        # Calculate Response Time
+        response_time = round(end_time - start_time, 3)
+
+        # Validate Status Code
+        if response.status_code == 200:
+
+            print("[green]✓ API Request Successful[/green]")
+
+            data = response.json()
+
+            print("\n[cyan]First User Details:[/cyan]")
+            print(f"Name : {data[0]['name']}")
+            print(f"Email: {data[0]['email']}")
+
+            print(
+                f"\n[bold blue]Response Time:[/bold blue] {response_time} seconds"
+            )
+
+        else:
+
+            print("[red]✗ API Request Failed[/red]")
+            print(f"Status Code: {response.status_code}")
+
+    except Exception as e:
+
+        print(f"[bold red]Error:[/bold red] {e}")
+
+
+
+check_api()
